c0cced863c32dbc660d615157c56a90fbb370159
apolloclark <apolloclark@gmail.com>
2018-01-07 21:10:11 -0500

    build_virtualbox.sh, added steps to remove existing OVF files

--- build_virtualbox.sh
+++ build_virtualbox.sh
@@ -3,14 +3,17 @@ global_start=`date +%s`
 
 # build Virtualbox ubuntu image
 cd ../packer-ubuntu
+rm -rf ./output-*-virtualbox-iso
 source ./build_ubuntu1604-server.sh
 
 # build Virtualbox base image
 cd ../packer-aws-base
+rm -rf ./output-*-virtualbox-iso
 source ./build_packer_virtualbox.sh
 
 # build Virtualbox webapp image
 cd ../packer-aws-webapp
+rm -rf ./output-*-virtualbox-iso
 source ./build_packer_virtualbox.sh
 
 
