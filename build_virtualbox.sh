4cfc3ea87f8db68c3a2876db72e27ca9f1b9b14f
apolloclark <apolloclark@gmail.com>
2018-01-09 18:15:01 -0500

    added ELK image to build script

--- build_virtualbox.sh
+++ build_virtualbox.sh
@@ -17,6 +17,10 @@ source ./build_packer_virtualbox.sh
 cd ../packer-aws-java
 source ./build_packer_virtualbox.sh
 
+# build Virtualbox ELK image
+cd ../packer-aws-elk
+source ./build_packer_virtualbox.sh
+
 global_end=`date +%s`
 secs=$((global_end-global_start))
 printf 'runtime = %02dh:%02dm:%02ds\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60))
