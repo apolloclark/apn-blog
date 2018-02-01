13740ce5c49a75f67a17a0e1966899bdbabab24d
apolloclark <apolloclark@gmail.com>
2018-01-31 22:37:48 -0500

    fixed issue with webapp/userdata script failing to run rvm

--- terraform/webapp/userdata.sh
+++ terraform/webapp/userdata.sh
@@ -1,10 +1,4 @@
-#!/bin/bash -eux
-
-# This script exists until Ansible is able to natively read from the
-# AWS SSM Parameter Store.
-# https://docs.ansible.com/ansible/devel/module_docs/aws_ssm_parameter_store_module.html
-# As of 2018-01-30, the aws_ssm_parameter_store module can only write, edit,
-# and delete keys
+#!/bin/bash -e
 
 # fix missing Apache page
 mkdir -p /var/www/html/public
@@ -12,32 +6,22 @@ cp /var/www/html/index.html /var/www/html/public/index.html
 
 # set the HOME variable
 # https://github.com/ansible/ansible/issues/31617
+printenv
 export HOME=/root
-source ~/.profile
-
-# install Ansible
-pip install ansible
+export _system_name="Ubuntu"
+source /usr/local/rvm/scripts/rvm
+gem install serverspec
 
 # download the packer-aws-elk-monitoring project
-pwd
-rm -rf ./packer-aws-elk-monitoring/
-git clone https://github.com/apolloclark/packer-aws-elk-monitoring
-cd packer-aws-elk-monitoring/ansible
-
-# retrieve SSM Parameter Store secret values
-ELK_PRIVATE_IP=$(aws ssm get-parameters --name "/elk/elk-ec2_private_ip" \
-  --with-decryption --query "Parameters[].Value"  --output text);
-
-# update vars_ssm.yml
-cat <<EOF > vars_ssm.yml
----
-hostname: all
-
-elastic_private_ip: $ELK_PRIVATE_IP
-EOF
+git clone https://github.com/apolloclark/packer-aws-elk-monitoring /root/packer-aws-elk-monitoring
+cd /root/packer-aws-elk-monitoring/ansible
 
 # download the ansible playbooks into the "roles" folder
 ansible-galaxy install --force -v --roles-path='./roles' --role-file='./requirements.yml'
 
 # run the playbook, against localhost
 ansible-playbook playbook.yml
+
+# run serverspec tests
+cd ../
+rake spec
