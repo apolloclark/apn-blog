b9f7fc5cde64bc3c54163f89c5aed816de68ff76
Apollo Clark <apolloclark@gmail.com>
2018-07-06 15:19:30 -0400

    added steps to check if the host system has the required executables

--- single_step_deploy.sh
+++ single_step_deploy.sh
@@ -2,6 +2,40 @@
 
 # single-step deploy script, just to prove that it's possible ;)
 
+
+
+# check if aws-cli is installed
+if ! [ -x "$(command -v aws)" ]; then
+	echo "[ERROR] aws-cli is not installed: https://docs.aws.amazon.com/cli/latest/userguide/installing.html"
+	exit 1;
+fi
+
+# check if packer is installed
+if ! [ -x "$(command -v packer)" ]; then
+	echo "[ERROR] packer is not installed: https://www.packer.io/downloads.html"
+	exit 1;
+fi
+
+# check if ansible is installed
+if ! [ -x "$(command -v ansible)" ]; then
+	echo "[ERROR] ansible is not installed: https://docs.ansible.com/ansible/2.3/intro_installation.html"
+	exit 1;
+fi
+
+# check if serverspec is installed
+if ! [ -x "$(command -v rake)" ]; then
+	echo "[ERROR] serverspec is not installed: https://serverspec.org/"
+	exit 1;
+fi
+
+# check if terraform is installed
+if ! [ -x "$(command -v terraform)" ]; then
+	echo "[ERROR] terraform is not installed: https://www.terraform.io/downloads.html"
+	exit 1;
+fi
+
+
+
 # create an EC2 keypair named "packer"
 aws ec2 create-key-pair --key-name packet --query "KeyMaterial" \
    --output text > ~/.ssh/packer.pem
@@ -11,6 +45,8 @@ chmod 0600 ~/.ssh/packer.pem
 
 # add the newly created key to the keychain
 ssh-add ~/.ssh/packer.pem
+
+
 
 # clone the project, and sub-modules
 git clone --recurse-submodules https://github.com/apolloclark/tf-aws
