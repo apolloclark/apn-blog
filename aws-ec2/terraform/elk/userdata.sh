532a8a3074bbb888ff79f8ec2855d29c60e3efec
apolloclark <apolloclark@gmail.com>
2018-12-30 23:46:45 -0500

    fixed serverspec tests in cloud-init / userdata.sh script

--- aws-ec2/terraform/elk/userdata.sh
+++ aws-ec2/terraform/elk/userdata.sh
@@ -5,7 +5,7 @@ export HOME=/root
 
 # download the packer-aws-elk-monitoring project
 git clone https://github.com/apolloclark/packer-aws-elk /root/packer-aws-elk
-cd /root/packer-aws-elk/config/ansible
+cd ./packer-aws-elk/config/ansible
 
 # download the ansible playbooks into the "roles" folder
 ansible-galaxy install --force -v --roles-path='./roles' --role-file='./requirements.yml'
@@ -14,6 +14,6 @@ ansible-galaxy install --force -v --roles-path='./roles' --role-file='./requirem
 ansible-playbook playbook.yml
 
 # run serverspec tests
-cd ../
-source /usr/local/rvm/scripts/rvm
+cd ../serverspec
+source /home/root/.rvm/scripts/rvm
 rake spec
--- aws-ec2/terraform/webapp/userdata.sh
+++ aws-ec2/terraform/webapp/userdata.sh
@@ -5,17 +5,17 @@ export HOME=/root
 
 # download the packer-aws-elk-monitoring project
 git clone https://github.com/apolloclark/packer-aws-webapp /root/packer-aws-webapp
-cd /root/packer-aws-webapp/config/ansible
+cd ./packer-aws-webapp/config/ansible
 
 # download the ansible playbooks into the "roles" folder
 ansible-galaxy install --force -v --roles-path='./roles' --role-file='./requirements.yml'
 
 # run the playbook, against localhost
-ansible-playbook playbook_local.yml
+ansible-playbook playbook.yml
 
 # run serverspec tests
-cd ../
-source /usr/local/rvm/scripts/rvm
+cd ../serverspec
+source /home/root/.rvm/scripts/rvm
 rake spec
 
 # add custom steps to install your website here
